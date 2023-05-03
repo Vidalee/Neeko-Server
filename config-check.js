1008fdc3c882fa03ae34eee24e5f9f22e4afbdd7
Vivi <vivi@vivide.re>
2023-05-03 22:16:24 +0200

    chore(config): remove temporary api key 🙂

--- config-check.js
+++ config-check.js
@@ -1,6 +1,6 @@
 const fs = require('fs');
 
 if (!fs.existsSync('./config.json')) {
-    console.error(`No config.json found! Use the config.json.example from Vidalee/Neeko-Server and rename it.`);
+    console.error(`No config.json found! Use the config.example.json from Vidalee/Neeko-Server and rename it.`);
     process.exit(1);
 }
